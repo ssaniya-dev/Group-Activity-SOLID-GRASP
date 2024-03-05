4838ed5f160cb116811806a7aa66174de361a8a6
Saniya Savla <ssaniyasavla@gmail.com>
2024-03-04 19:48:58 -0500

    Update Task.java

--- Task.java
+++ Task.java
@@ -1,31 +1,17 @@
 import java.time.LocalDate;
 
-public interface Task {
-    String getTitle();
-    void setTitle(String title);
-    String getDescription();
-    void setDescription(String description);
-    LocalDate getDueDate();
-    void setDueDate(LocalDate dueDate);
-    String getStatus();
-    void setStatus(String status);
-    int getPriority();
-    void setPriority(int priority);
+public abstract class Task {
+    protected String title;
+    protected String description;
+    protected LocalDate dueDate;
+    protected String status;
+    protected int priority;
 }
 
-public class RecurringTask implements Task {
-    private String title;
-    private String description;
-    private LocalDate dueDate;
-    private String status;
-    private int priority;
+public class RecurringTask extends Task {
     private LocalDate recurrence;
 }
 
-public class RegularTask implements Task {
-    private String title;
-    private String description;
-    private LocalDate dueDate;
-    private String status;
-    private int priority;
+public class RegularTask extends Task {
+   
 }
